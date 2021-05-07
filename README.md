a5b1b93b59dddd7009df20c24c2b0b08c9ca2746
hdvos <hdvos93@gmail.com>
2021-05-07 22:21:45 +0200

    logic is finished

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Planned features (not in order of priority)
 - Automatic tagging using machine learning
 - Automatic plotting on a map
 - building an index
+- re-evaluate entire tree
+- set up more elaborate metadata database
+- possibility to process files in bulk instead of re-opening the index every time etc.
+
+
+Candidate fields for index:
+- Tags
+- metadata link
+
+
+**Disclaimer: I designed this solely for personal use so no guarantees. But use whatever you want.**
--- scripts/sort_fotos.py
+++ scripts/sort_fotos.py
@@ -10,6 +10,10 @@ from typing import Union
 
 from datetime import datetime
 
+from csv import writer as CsvWriter
+
+import shutil
+
 IMAGE_FOLDER = "/home/hugo/MEGA/fotos/pictures"
 
 
@@ -18,6 +22,18 @@ image_filenames = [os.path.join(IMAGE_FOLDER, filename) for filename in image_fi
 
 #TODO: dingen met GPS
 
+#%% Utils
+def print_exif(exifdata):
+    for tag_id in exifdata:
+        # get the tag name, instead of human unreadable tag id
+        tag = TAGS.get(tag_id, tag_id)
+        data = exifdata.get(tag_id)
+        # decode bytes 
+        if isinstance(data, bytes):
+            data = data.decode()
+        print(f"{tag:25}({tag_id}): {data}")
+
+#%% Datastructures
 @dataclass
 class MyGpsInfo():
     GPSAltitude: tuple
@@ -27,6 +43,8 @@ class MyGpsInfo():
     GPSLongitude: tuple
     GPSLongitudeRef: Union[bytes, str]
 
+
+#%% MyImage Class
 class MyImage():
     def __init__(self, filename:str):
         self.filename = filename
@@ -51,7 +69,7 @@ class MyImage():
                 )
         except:
             mygpsinfo = None
-            
+
         self.GpsData = mygpsinfo
 
         try:
@@ -59,29 +77,61 @@ class MyImage():
         except:
             self.DateTime = None
         
-    def organize(self, image_root_destination:str='../pictures', remove_source:bool=False):
+    def organize(self, image_root_destination:str='../pictures', remove_source:bool=False, update_self:bool = True):
         if not os.path.exists(image_root_destination):
             os.makedirs(image_root_destination)
         
+        year = str(self.DateTime.year)
+        month = self.DateTime.strftime('%m_%B')
 
-
-
-def print_exif(exifdata):
-    for tag_id in exifdata:
-        # get the tag name, instead of human unreadable tag id
-        tag = TAGS.get(tag_id, tag_id)
-        data = exifdata.get(tag_id)
-        # decode bytes 
-        if isinstance(data, bytes):
-            data = data.decode()
-        print(f"{tag:25}({tag_id}): {data}")
-
-
-for i, image_name in enumerate(image_filenames):
-    # print(i)
-    myimage = MyImage(image_name)
+        file_destination_folder = os.path.join(image_root_destination, year, month)
     
-    # image = Image.open(image_name)
+        if not os.path.exists(file_destination_folder):
+            os.makedirs(file_destination_folder)
 
-    # input("-"*50)
-    
+        file_path = os.path.join(file_destination_folder, os.path.basename(self.filename))
+        # input(file_path)
+        if remove_source:
+            shutil.move(self.filename, file_path)
+        else:
+            shutil.copy2(self.filename, file_path)
+
+        if update_self:
+            self.filename = file_path
+
+    #TODO: adapt as to easily add new fieldnames
+    def _initialize_index_file(self, index_file:str):
+        assert not os.path.exists(index_file), 'Cannot initialize, file already exists.'
+
+        with open(index_file, 'wt') as out:
+            writer = CsvWriter(out, delimiter='\t')
+            writer.writerow(["Description", "Filename", "Location", "DateTime"])
+
+    #TODO: adapt as to easily add new fieldnames
+    def add_to_index(self, index_file:str = "../index.csv"):
+        if not os.path.exists(index_file):
+            self._initialize_index_file(index_file)
+
+        description = ''
+        filename = os.path.basename(self.filename)
+        location = self.filename
+        date_and_time = self.DateTime.strftime('%Y:%m:%d %H:%M:%S')
+
+        with open(index_file, 'a') as out:
+            writer = CsvWriter(out, delimiter='\t')
+            writer.writerow([description, filename, location, date_and_time])
+
+
+#%% Main
+if __name__ == '__main__':
+    os.chdir(os.path.split(__file__)[0])   
+    assert os.getcwd() == os.path.split(__file__)[0]
+    for i, image_name in enumerate(image_filenames):
+        # print(i)
+        myimage = MyImage(image_name)
+        myimage.organize()
+        myimage.add_to_index()
+        # print(os.getcwd())
+        # image = Image.open(image_name)
+
+        # input("-"*50)
